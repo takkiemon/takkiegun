ceb41bee2029c51b3ae29abe375aa216732d7e90
DESKTOP-112MOQR\takki <takkie91@hotmail.com>
2019-09-10 23:41:44 +0200

    add shooting, added some numbers to the shooting and adding dev scene

--- TakkieGun/Assets/Scripts/GunController.cs
+++ TakkieGun/Assets/Scripts/GunController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
+    // the offset of the barrel relative to the rest of the gun (mostly relative to the center (of gravity))
+    public Vector3 gunBarrelOffset; // let's assume that the gun is pointing to the left (as if you're holding a gun with your right hand and inspecting it)
+    public float gunGizmoRadius;
+    public float shootingForce;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,21 @@ public class GunController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 shootingDirection = new Vector3(-gunBarrelOffset.x, 0, 0);
+        GetComponent<Rigidbody>().AddForceAtPosition(shootingDirection.normalized * shootingForce, transform.position + gunBarrelOffset);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position + gunBarrelOffset, gunGizmoRadius);
     }
 }
