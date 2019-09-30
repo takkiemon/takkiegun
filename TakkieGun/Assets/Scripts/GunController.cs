8050dc2f7bd5d4bc23416410b1fd7699f0ac6851
takkiemon <takkie91@hotmail.com>
2019-09-30 17:19:31 +0200

    wip see previous commit

--- TakkieGun/Assets/Scripts/GunController.cs
+++ TakkieGun/Assets/Scripts/GunController.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class GunController : MonoBehaviour
 {
     // the offset of the barrel relative to the rest of the gun (mostly relative to the center (of gravity))
-    public Vector3 gunbarrelRedOffset; // let's assume that the gun is pointing to the left (as if you're holding a gun with your right hand and inspecting it)
-    public Vector3 gunbarrelGreenOffset; // the other barrel
-    public Vector3 gunbarrelBlueOffset; // let's assume that the gun is pointing to the left (as if you're holding a gun with your right hand and inspecting it)
-    public Vector3 gunbarrelYellowOffset; // the other barrel
+    public Vector3 shipThrusterRedOffset; // let's assume that the gun is pointing to the left (as if you're holding a gun with your right hand and inspecting it)
+    public Vector3 shipThrusterGreenOffset; // the other barrel
+    public Vector3 shipThrusterBlueOffset; // let's assume that the gun is pointing to the left (as if you're holding a gun with your right hand and inspecting it)
+    public Vector3 shipThrusterYellowOffset; // the other barrel
 
     public float[] offSetHeights; // this array holds the y-values that changes the offsets of the two barrels (for now I only want to change one dimension)
     public Vector3 gravityVector;
@@ -18,7 +18,7 @@ public class GunController : MonoBehaviour
     public int greenPosition;
 
     public float gunGizmoRadius;
-    public float shootingForce;
+    public float thrustingForce;
 
     public ParticleSystem jetRed;
     public ParticleSystem jetGreen;
@@ -28,54 +28,65 @@ public class GunController : MonoBehaviour
     public float maxFuel;
     public float currentFuel;
 
-    public GameObject barrelRed;
-    public GameObject barrelGreen;
-    public GameObject barrelBlue;
-    public GameObject barrelYellow;
+    public GameObject[] thrusters;
+    public GameObject thrusterRed;
+    public GameObject thrusterGreen;
+    public GameObject thrusterBlue;
+    public GameObject thrusterYellow;
 
     public Text fuelText;
 
     private Rigidbody rb;
+    private Vector3[] thrusterAxisPositions;
+    private Vector3[] thrusterDiagonalPositions;
+    private Vector3[] thrusterAxisEulers;
+    private Vector3[] thrusterDiagonalEulers;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         ApplySettings();
-        redPosition = 0;
-        greenPosition = 0;
+        //redPosition = 0;
+        //greenPosition = 0;
 
+        SetThrusterPositionVariables();
+        SetAxisThrusters();
+        SetDiagonalThrusters();
+
+        /*
         offSetHeights = new float[3];
         offSetHeights[0] = 0f;
         offSetHeights[1] = -0.25f;
         offSetHeights[2] = 0.25f;
+        */
     }
 
     [ContextMenu("Apply new values")]
     public void ApplySettings()
     {
-        barrelRed.transform.localPosition = gunbarrelRedOffset;
-        barrelGreen.transform.localPosition = gunbarrelGreenOffset;
+        thrusterRed.transform.localPosition = shipThrusterRedOffset;
+        thrusterGreen.transform.localPosition = shipThrusterGreenOffset;
         Physics.gravity = gravityVector;
         UpdateFuelText();
     }
 
-    public void ApplySettings(Vector3 redBarrelOffset, Vector3 greenBarrelOffset, Vector3 gravity)
+    public void ApplySettings(Vector3 redThrusterOffset, Vector3 greenThrusterOffset, Vector3 gravity)
     {
-        gunbarrelRedOffset = redBarrelOffset;
-        gunbarrelGreenOffset = greenBarrelOffset;
+        shipThrusterRedOffset = redThrusterOffset;
+        shipThrusterGreenOffset = greenThrusterOffset;
         gravityVector = gravity;
         ApplySettings();
     }
 
     public void ChangeRedPosition(float newOffset)
     {
-        gunbarrelRedOffset.y = newOffset;
+        shipThrusterRedOffset.y = newOffset;
     }
 
     public void ChangeGreenPosition(float newOffset)
     {
-        gunbarrelGreenOffset.y = newOffset;
+        shipThrusterGreenOffset.y = newOffset;
     }
 
     // Update is called once per frame
@@ -83,7 +94,7 @@ public class GunController : MonoBehaviour
     {
         if (Input.GetButton("FireRed"))
         {
-            Shoot(barrelRed.transform.localPosition, barrelRed, shootingForce);
+            Shoot(thrusterRed.transform.localPosition, thrusterRed, thrustingForce);
             if (!jetRed.isPlaying)
                 jetRed.Play();
         }
@@ -95,7 +106,7 @@ public class GunController : MonoBehaviour
 
         if (Input.GetButton("FireGreen"))
         {
-            Shoot(barrelGreen.transform.localPosition, barrelGreen, shootingForce);
+            Shoot(thrusterGreen.transform.localPosition, thrusterGreen, thrustingForce);
             if (!jetGreen.isPlaying)
             {
                 jetGreen.Play();
@@ -138,16 +149,16 @@ public class GunController : MonoBehaviour
         ApplySettings();
     }
 
-    void Shoot(Vector3 gunBarrelOffset, GameObject barrelObject, float shootingForce)
+    void Shoot(Vector3 gunThrusterOffset, GameObject thrusterObject, float thustingForce)
     {
-        Vector3 shootingDirection = new Vector3(-gunBarrelOffset.x, 0, 0); // the direction is always opposite to the direction the barrel is facing
+        Vector3 shootingDirection = new Vector3(-gunThrusterOffset.x, 0, 0); // the direction is always opposite to the direction the barrel is facing
 
         rb.AddForceAtPosition(
-            transform.TransformDirection(shootingDirection.normalized * shootingForce), // TransformDirection converts localspace vectors to worldspace values
-            barrelObject.transform.position
+            transform.TransformDirection(shootingDirection.normalized * thustingForce), // TransformDirection converts localspace vectors to worldspace values
+            thrusterObject.transform.position
             );
 
-        currentFuel -= shootingForce;
+        currentFuel -= thustingForce;
         UpdateFuelText();
     }
 
@@ -156,9 +167,68 @@ public class GunController : MonoBehaviour
         fuelText.text = "Fuel: " + currentFuel + "/" + maxFuel;
     }
 
-    private void OnDrawGizmos()
+    public void SetThrusterPositionVariables()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(barrelGreen.transform.position, gunGizmoRadius);
+        thrusterAxisPositions = new Vector3[4] 
+        { 
+            new Vector3(0, 0.5f, 0), //upper
+            new Vector3(0, -0.5f, 0), //lower
+            new Vector3(0.5f, 0, 0), //right
+            new Vector3(-0.5f, 0, 0) //left
+        };
+
+        thrusterAxisEulers = new Vector3[4]
+        {
+            new Vector3(0, 0, 180), //upper
+            new Vector3(0, 0, 0), //lower
+            new Vector3(0, 0, -90), //right
+            new Vector3(0, 0, 90) //left
+        };
+
+        thrusterDiagonalPositions = new Vector3[4] 
+        { 
+            new Vector3(0.5f, 0.5f, 0), //upper-right
+            new Vector3(0.5f, -0.5f, 0), //lower-right
+            new Vector3(-0.5f, -0.5f, 0), //lower-left
+            new Vector3(-0.5f, 0.5f, 0) //upper-left
+        };
+
+        thrusterDiagonalEulers = new Vector3[4]
+        {
+            new Vector3(0, 0, -135), //upper-right
+            new Vector3(0, 0, -45), //lower-right
+            new Vector3(0, 0, 45), //lower-left
+            new Vector3(0, 0, 135) //upper-left
+        };
+    }
+
+    public void SetAxisThrusters()
+    {
+        for (int i = 0; i < thrusters.Length; i++)
+        {
+            if (thrusters[i] != null)
+                PlaceAxisThrusters(thrusters[i], i);
+        }
+    }
+
+    public void SetDiagonalThrusters()
+    {
+        for (int i = 0; i < thrusters.Length; i++)
+        {
+            if (thrusters[i] != null)
+                PlaceDiagonalThrusters(thrusters[i], i);
+        }
+    }
+
+    public void PlaceAxisThrusters(GameObject thruster, int positionIndex)
+    {
+        thruster.transform.localPosition = thrusterAxisPositions[positionIndex];
+        thruster.transform.eulerAngles = thrusterAxisEulers[positionIndex];
+    }
+
+    public void PlaceDiagonalThrusters(GameObject thruster, int positionIndex)
+    {
+        thruster.transform.localPosition = thrusterDiagonalPositions[positionIndex];
+        thruster.transform.eulerAngles = thrusterDiagonalEulers[positionIndex];
     }
 }
--- TakkieGun/Assets/Scripts/Jet.cs
+++ TakkieGun/Assets/Scripts/Jet.cs
@@ -6,7 +6,7 @@ public class Jet : MonoBehaviour
 {
     public Vector3 gunbarrelOffset;
 
-    public int position;
+    //public int position;
 
     public float shootingForce;
 
@@ -16,12 +16,23 @@ public class Jet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.localPosition = gunbarrelOffset;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Place(Vector3 position)
+    {
+        transform.localPosition = position;
+    }
+
+    public void Place(Vector3 position, Vector3 rotation)
+    {
+        transform.localPosition = position;
+        transform.eulerAngles = rotation;
     }
 }
