e1bdb8605e94bc17b069f8a11f2520e026345aa6
takkiemon <takkie91@hotmail.com>
2019-10-05 18:08:32 +0200

    wip: creating obstacle events

--- TakkieGun/Assets/Scripts/ObstacleSripts/GroupPattern.cs
+++ TakkieGun/Assets/Scripts/ObstacleSripts/GroupPattern.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class GroupPattern : MonoBehaviour
 {
+    public WallPatternManager patternManager;
+    public GameObject[] movingWalls = new GameObject[3];
+    private Vector3 defaultWallPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        defaultWallPosition = new Vector3(0, 16, 0);
+
+        foreach (GameObject wall in movingWalls)
+        {
+            wall.transform.position = defaultWallPosition;
+        }
     }
 
     // Update is called once per frame
--- TakkieGun/Assets/Scripts/ObstacleSripts/WallPatternManager.cs
+++ TakkieGun/Assets/Scripts/ObstacleSripts/WallPatternManager.cs
@@ -8,23 +8,50 @@ public class WallPatternManager : MonoBehaviour
     public DoublePattern doubleScript;
     public GroupPattern groupScript;
 
+    private int currentPattern;
+
     public GameObject[] movingWalls = new GameObject[5];
     private Vector3 defaultWallPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        defaultWallPosition = new Vector3(0, 16, 0);
 
-        foreach(GameObject wall in movingWalls)
-        {
-            wall.transform.position = defaultWallPosition;
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartPattern(int patternNumber)
+    {
+        currentPattern = patternNumber;
+        switch(patternNumber)
+        {
+            case 0:
+                oneWayScript.enabled = false;
+                doubleScript.enabled = false;
+                groupScript.enabled = false;
+                break;
+            case 1:
+                oneWayScript.enabled = true;
+                doubleScript.enabled = false;
+                groupScript.enabled = false;
+                break;
+            case 2:
+                oneWayScript.enabled = false;
+                doubleScript.enabled = true;
+                groupScript.enabled = false;
+                break;
+            case 3:
+                oneWayScript.enabled = false;
+                doubleScript.enabled = false;
+                groupScript.enabled = true;
+                break;
+            default:
+                break;
+        }
     }
 }
